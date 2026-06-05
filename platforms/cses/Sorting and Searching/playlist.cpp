2319d89faf8569dc62c04dffea85d3d1650cedf5
Luciano Kohler da Silva <lucianokohlerdasilva@gmail.com>
2026-06-05 13:01:46 -0300

    Playlist

--- platforms/cses/Sorting and Searching/playlist.cpp
+++ platforms/cses/Sorting and Searching/playlist.cpp
@@ -1,22 +1,52 @@
 // https://cses.fi/problemset/task/1141/
-
-#include <bits/stdc++.h>
-#define ll long long
+#include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+const ll modulo = 1e9+7;
+const int mx = 2e5+5;
 
-int main()
-{
-  int n;
-  cin >> n;
-  vector<ll> songs;
-  while(n--){
-    ll song;
-    cin >> song;
-    auto it = find(songs.begin(), songs.end(), song);
-    if(it == songs.end()){
-      songs.push_back(song);
+
+void solve(){
+    int n; cin >> n;
+    vector<int>nums(n, 0);
+    map<int, int>present;
+
+    for(int i = 0; i < n; i++) cin >> nums[i];
+
+    if(n == 1){
+      cout << "1\n";
+      return;
     }
-  }
 
-  cout << songs.size();
+    int l = 0, r = 0;
+    present[nums[l]]++;
+    int maxVal = 0, curr = 1;
+    while(r < n){
+      r++;
+      curr++;
+
+      if(r == n){
+        break;
+      }
+
+      if(present[nums[r]]){ // Already present
+        while(present[nums[r]]){
+          present[nums[l]]--;
+          l++;
+          curr--;
+        }
+      }
+
+      present[nums[r]]++;
+      maxVal = max(maxVal, curr);
+    }
+
+    cout << maxVal << "\n";
+}
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    solve();
+
+    return 0;
 }
